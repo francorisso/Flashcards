e053fb9b743324301df1ed032177a029f2a2e234
Franco Risso <franco@720desarrollos.com>
2016-11-16 11:09:59 -0300

    fix server issues + add images

--- src/ducks/vocabulary.js
+++ src/ducks/vocabulary.js
@@ -76,6 +76,192 @@ export function load() {
       name: 'das Bett (e)',
       image: 'bett_d4nbxb',
     },
+    {
+      name: 'die Kette (e)',
+      image: 'kette_izh1e6',
+    },
+    {
+      name: 'der Fotoapparat (e)',
+      image: 'fotoapparat_eqk6xg',
+    },
+    {
+      name: 'das Feuerzeug (e)',
+      image: 'feuerzeug_jtq4ur',
+    },
+    {
+      name: 'die Flasche (e)',
+      image: 'flasche_aj6qpn',
+    },
+    {
+      name: 'das Buch (e)',
+      image: 'buch_jthuv2',
+    },
+    {
+      name: 'die Brille (e)',
+      image: 'brille_so8qin',
+    },
+    {
+      name: 'der Bleistift (e)',
+      image: 'bleistift_glbme2',
+    },
+    {
+      name: 'der Schlüssel (e)',
+      image: 'schlusel_uubyqi',
+    },
+    {
+      name: 'der Kugelschreiber (e)',
+      image: 'kugelschreiber_hpmrn3',
+    },
+    {
+      name: 'das Uhr (e)',
+      image: 'uhr_mkxnjp',
+    },
+    {
+      name: 'der Schirm (e)',
+      image: 'schirm_kamqsv',
+    },
+    {
+      name: 'die Tasche (e)',
+      image: 'tasche_utb0tv',
+    },
+    {
+      name: 'der Streichholz (e)',
+      image: 'streichholz_bvg7y7',
+    },
+    {
+      name: 'die Seife (e)',
+      image: 'steife_wfub1c',
+    },
+    {
+      name: 'die Geldbörse (e)',
+      image: 'geldborse_jg5zyl',
+    },
+
+    {
+      name: 'der Schinken (e)',
+      image: 'schinken_bdavta',
+    },
+    {
+      name: 'der Apfel (e)',
+      image: 'apfel_paruch',
+    },
+    {
+      name: 'die Butter (e)',
+      image: 'butter_pf8xac',
+    },
+    {
+      name: 'der Fisch (e)',
+      image: 'fisch_ksyagp',
+    },
+    {
+      name: 'der Kuchen (e)',
+      image: 'kuchen_p55nbx',
+    },
+    {
+      name: 'die Kartoffel (e)',
+      image: 'kartoffel_iqjgit',
+    },
+    {
+      name: 'der Braten (e)',
+      image: 'braten_ltg5v0',
+    },
+    {
+      name: 'der Käse (e)',
+      image: 'kese_eamkjt',
+    },
+    {
+      name: 'die Orange (e)',
+      image: 'orange_ukzpn7',
+    },
+    {
+      name: 'der Salat (e)',
+      image: 'salat_dq5s8j',
+    },
+    {
+      name: 'die Suppe (e)',
+      image: 'suppe_im0yqo',
+    },
+    {
+      name: 'die Milch (e)',
+      image: 'milch_sc3avc',
+    },
+    {
+      name: 'die Schokolade (e)',
+      image: 'schokolade_frb5f3',
+    },
+    {
+      name: 'der Tee (e)',
+      image: 'tee_sm7gcp',
+    },
+    {
+      name: 'die Tomate (e)',
+      image: 'tomate_b1xxre',
+    },
+    {
+      name: 'das Brot (e)',
+      image: 'brot_k8yrul',
+    },
+
+    {
+      name: 'der Computer (e)',
+      image: 'computer_enot6c',
+    },
+    {
+      name: 'die SMS (e)',
+      image: 'sms_ndoxxq',
+    },
+    {
+      name: 'das Telefon (e)',
+      image: 'telefon_dkp5uw',
+    },
+    {
+      name: 'der Drucker (e)',
+      image: 'drucker_olfuue',
+    },
+    {
+      name: 'das Handy (e)',
+      image: 'handy_xdennl',
+    },
+    {
+      name: 'der Kalender (e)',
+      image: 'kalender_w33ycj',
+    },
+    {
+      name: 'das Formular (e)',
+      image: 'formulare_qafhtw',
+    },
+    {
+      name: 'der Laptop (e)',
+      image: 'laptop_ldzegi',
+    },
+    {
+      name: 'die E-mail (e)',
+      image: 'e-mail_pjxfzf',
+    },
+    {
+      name: 'das Notizbuch (e)',
+      image: 'notizbuch_ooc6hq',
+    },
+    {
+      name: 'der Bildschirm (e)',
+      image: 'bildschirm_zamddx',
+    },
+    {
+      name: 'die Maus (e)',
+      image: 'maus_fbcpeq',
+    },
+    {
+      name: 'die Briefmarke (e)',
+      image: 'briefmarke_xvaqeh',
+    },
+    {
+      name: 'die Rechnung (e)',
+      image: 'rechnung_bsyqtw',
+    },
+    {
+      name: 'der Stift (e)',
+      image: 'stift_aorxra',
+    },
   ];
   words = words.map((word, idx) => ({
     ...word,
--- tools/server.js
+++ tools/server.js
@@ -3,12 +3,12 @@ import WebpackDevServer from 'webpack-dev-server';
 import config from '../webpack.config.dev';
 import startMessage from './server_start_message';
 
-const port = 8000;
+const port = 3000;
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
 }).listen(port, 'localhost', function (err) {
   if (err) {
     return console.error(err);
